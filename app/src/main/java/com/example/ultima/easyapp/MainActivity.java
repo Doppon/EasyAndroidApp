dfdfeb0a2f150364b9552a0b375f2e437992a72a
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-01-27 21:11:25 +0900

    add event

--- app/src/main/java/com/example/ultima/easyapp/MainActivity.java
+++ app/src/main/java/com/example/ultima/easyapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ultima.easyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeMessageText(View view) {
+        TextView tv = (TextView)findViewById(R.id.messagetext);
+        tv.setText("SUCCESS!!");
     }
 }
